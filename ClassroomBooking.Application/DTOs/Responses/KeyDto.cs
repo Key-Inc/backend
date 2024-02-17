c99995f6a1fa9a476bd8e98c827ad5b7664527c2
Ivan Semenov <devsemtsk@gmail.com>
2024-02-17 14:22:19 +0700

    Update KeyDto

--- ClassroomBooking.Application/DTOs/Responses/KeyDto.cs
+++ ClassroomBooking.Application/DTOs/Responses/KeyDto.cs
@@ -2,12 +2,11 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ClassroomBooking.Application.DTOs.Responses;
 
-public sealed class KeyDto
+public class KeyDto
 {
     [Required]
     public required Guid Id { get; set; }
     
     [Required]
-    [Range(0, int.MaxValue)]
-    public required int ClassroomNumber { get; set; }
+    public required Guid ClassroomId { get; set; }
 }
