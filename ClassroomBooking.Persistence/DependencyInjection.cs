9ca2d51baf189376d55c8fa7c04591ed26f8b67a
maksim <mazhur04@gmail.com>
2024-02-17 16:01:12 +0700

    Add docker-compose.yml

--- ClassroomBooking.Persistence/DependencyInjection.cs
+++ ClassroomBooking.Persistence/DependencyInjection.cs
@@ -11,6 +11,15 @@ public static class DependencyInjection
     {
         services.AddApplicationDbContext(configuration);
     }
+    
+    public static void AddAutoMigration(this IServiceProvider services)
+    {
+        using (var scope = services.CreateScope())
+        {
+            var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            context.Database.Migrate();
+        }
+    }
 
     private static void AddApplicationDbContext(this IServiceCollection services, IConfiguration configuration)
     {
--- ClassroomBooking.Web/Program.cs
+++ ClassroomBooking.Web/Program.cs
@@ -20,6 +20,8 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.Services.AddAutoMigration();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
