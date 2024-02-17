73a86175b95bae0e96be5aa674d99817f5bda486
Ivan Semenov <devsemtsk@gmail.com>
2024-02-17 19:58:11 +0700

    Add policy

--- ClassroomBooking.Infrastructure/DependencyInjection.cs
+++ ClassroomBooking.Infrastructure/DependencyInjection.cs
@@ -1,5 +1,7 @@
 using System.IdentityModel.Tokens.Jwt;
 using ClassroomBooking.Application.Common.Interfaces.Services;
+using ClassroomBooking.Domain.Entities.Enums;
+using ClassroomBooking.Infrastructure.Models;
 using ClassroomBooking.Infrastructure.Options.Configurations;
 using ClassroomBooking.Infrastructure.Services.Auth;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -12,10 +14,20 @@ public static class DependencyInjection
     public static void AddInfrastructureLayer(this IServiceCollection services)
     {
         services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer();
+        services.AddAuthorization();
         services.AddOptionConfigurations();
         services.AddHelpers();
         services.AddServices();
     }
+
+    private static void AddAuthorization(this IServiceCollection services)
+    {
+        services.AddAuthorizationBuilder()
+            .AddPolicy(Policy.Admin, policy => Policy.Configure(policy, UserRole.Admin))
+            .AddPolicy(Policy.Dean, policy => Policy.Configure(policy, UserRole.Dean))
+            .AddPolicy(Policy.Student, policy => Policy.Configure(policy, UserRole.Student))
+            .AddPolicy(Policy.Teacher, policy => Policy.Configure(policy, UserRole.Teacher));
+    } 
 
     private static void AddOptionConfigurations(this IServiceCollection services)
     {
