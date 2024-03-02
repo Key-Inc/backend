3f2e34ad0711bd5de6c91abdbe5a4f413556e76a
maksim <mazhur04@gmail.com>
2024-03-02 14:07:21 +0700

    Change key request entity

--- ClassroomBooking.Domain/Entities/KeyRequest.cs
+++ ClassroomBooking.Domain/Entities/KeyRequest.cs
@@ -8,8 +8,6 @@ public sealed class KeyRequest : BaseEntity
     public required RequestStatus Status { get; set; }
     public required DateTime StartDate { get; set; }
     public required DateTime EndDate { get; set; }
-    public required bool IsRecurring { get; set; }
-    
     public User? User { get; set; }
     public required Guid UserId { get; set; }
     
