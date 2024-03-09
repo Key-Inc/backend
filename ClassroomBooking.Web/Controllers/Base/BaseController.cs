b36588f90fbc63fd9811e007e912da877128f161
maksim <mazhur04@gmail.com>
2024-03-09 21:09:58 +0700

    Fix CORS

--- ClassroomBooking.Web/Controllers/Base/BaseController.cs
+++ ClassroomBooking.Web/Controllers/Base/BaseController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace ClassroomBooking.Web.Controllers.Base;
 
 [ApiController]
-[EnableCors("MyCors")]
+[DisableCors]
 [Route("api/[controller]")]
 public abstract class BaseController : ControllerBase
 {
