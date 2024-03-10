c50fca7f407880560a23c55d505cc6b7478504b4
Ivan Semenov <devsemtsk@gmail.com>
2024-03-10 12:06:14 +0700

    Add RejectTransferKeyRequestCommand

--- ClassroomBooking.Persistence/Repositories/TransferKeyRequestRepository.cs
+++ ClassroomBooking.Persistence/Repositories/TransferKeyRequestRepository.cs
@@ -1,10 +1,14 @@
 using ClassroomBooking.Application.Common.Interfaces.Repositories;
 using ClassroomBooking.Domain.Entities;
 using ClassroomBooking.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
 
 namespace ClassroomBooking.Persistence.Repositories;
 
 internal sealed class TransferKeyRequestRepository : BaseRepository<TransferKeyRequest>, ITransferKeyRequestRepository
 {
     public TransferKeyRequestRepository(ApplicationDbContext dbContext) : base(dbContext) {}
+    
+    public async Task<TransferKeyRequest?> GetByRecipientIdAndKeyId(Guid recipientId, Guid keyId) =>
+        await Entities.FirstOrDefaultAsync(request => request.RecipientId == recipientId && request.KeyId == keyId);
 }
