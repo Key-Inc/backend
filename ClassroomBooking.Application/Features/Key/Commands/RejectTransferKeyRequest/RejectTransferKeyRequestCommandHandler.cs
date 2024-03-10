b8334cf485e9cca2502db1e5c6805ca820ea077b
Ivan Semenov <devsemtsk@gmail.com>
2024-03-10 13:32:31 +0700

    Fix rejection

--- ClassroomBooking.Application/Features/Key/Commands/RejectTransferKeyRequest/RejectTransferKeyRequestCommandHandler.cs
+++ ClassroomBooking.Application/Features/Key/Commands/RejectTransferKeyRequest/RejectTransferKeyRequestCommandHandler.cs
@@ -2,6 +2,7 @@ using ClassroomBooking.Application.Common.Exceptions.Base;
 using ClassroomBooking.Application.Common.Interfaces.Repositories;
 using ClassroomBooking.Domain.Entities.Enums;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace ClassroomBooking.Application.Features.Key.Commands.RejectTransferKeyRequest;
 
@@ -16,9 +17,13 @@ public sealed class RejectTransferKeyRequestCommandHandler : IRequestHandler<Rej
 
     public async Task Handle(RejectTransferKeyRequestCommand request, CancellationToken cancellationToken)
     {
-        var transferRequest = await _transferKeyRequestRepository.GetByRecipientIdAndKeyId(request.UserId, request.KeyId);
+        var transferRequest = await _transferKeyRequestRepository.Entities
+            .FirstOrDefaultAsync(req =>
+                req.RecipientId == request.UserId && req.KeyId == request.KeyId &&
+                req.Status == RequestStatus.UnderConsideration, cancellationToken: cancellationToken);
+        
         if (transferRequest == null) 
-            throw new NotFoundException($"The request to transfer the key ({request.KeyId}) was not found");
+            throw new NotFoundException($"The request under consideration to transfer the key ({request.KeyId}) was not found");
 
         transferRequest.Status = RequestStatus.Rejected;
         await _transferKeyRequestRepository.UpdateAsync(transferRequest);
